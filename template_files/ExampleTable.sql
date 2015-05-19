ef2ae7d79ff14ebc3eba4d0f406ed2bf4737638b
unknown <pehe@pehe-764.sek.se>
2015-05-19 13:14:15 +0200

    Updated example so that it is an actual liquibase formatted sql. The parser is very sensitive to this.

--- template_files/ExampleTable.sql
+++ template_files/ExampleTable.sql
@@ -1,6 +1,6 @@
--- Liquibase formatted sql
+--liquibase formatted sql
 
--- changeset peter.henell:JIRA-01234-add-Area-table
+--changeset peter.henell:JIRA-01234-add-Area-table
 Create table CityArea(
 	CityAreaID		bigint identity(1, 1) constraint [PK_CityArea] primary key,
 	Description		varchar(500)
