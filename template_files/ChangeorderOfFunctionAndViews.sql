4827e74730e716b2e14e9688203c78bf6b138b49
unknown <pehe@pehe-764.sek.se>
2015-05-25 11:01:49 +0200

    Script now returns functions in the order that they must be created in.

--- template_files/ChangeorderOfFunctionAndViews.sql
+++ template_files/ChangeorderOfFunctionAndViews.sql
@@ -5,31 +5,43 @@ declare   @PARAM_OBJECT_NAME VARCHAR(500) = '';
     (
         SELECT DISTINCT 
         b.object_id AS UsedByObjectId, 
+		SCHEMA_NAME(b.schema_id) AS UseByObjectSchema,
         b.name AS UsedByObjectName, b.type AS UsedByObjectType, 
         c.object_id AS DependentObjectId, 
+		SCHEMA_NAME(c.schema_id) AS DependentObjectSchema,
         c.name AS DependentObjectName , c.type AS DependenObjectType
-        FROM  sys.sysdepends a
-        INNER JOIN sys.objects b ON a.id = b.object_id
-        INNER JOIN sys.objects c ON a.depid = c.object_id
-        WHERE b.type IN ('FN') AND c.type IN ('FN') -- , 'V'
+        FROM  sys.objects b
+        LEFT OUTER JOIN sys.sysdepends a ON a.id = b.object_id 
+        left outer JOIN sys.objects c ON a.depid = c.object_id AND c.type IN ('FN') -- , 'V
+		where b.type IN ('FN')
+        
     ),
     CTE_DependentObjects2 AS
     (
        SELECT 
           UsedByObjectId, UsedByObjectName, UsedByObjectType,
-          DependentObjectId, DependentObjectName, DependenObjectType, 
+          DependentObjectId, DependentObjectName, DependenObjectType, UseByObjectSchema, DependentObjectSchema,
           1 AS Level
        FROM CTE_DependentObjects a
-       --WHERE a.UsedByObjectName = @PARAM_OBJECT_NAME
+       WHERE a.DependentObjectId is NULL
+	   --WHERE a.UsedByObjectName = @PARAM_OBJECT_NAME
        UNION ALL 
        SELECT 
           a.UsedByObjectId, a.UsedByObjectName, a.UsedByObjectType,
-          a.DependentObjectId, a.DependentObjectName, a.DependenObjectType, 
+          a.DependentObjectId, a.DependentObjectName, a.DependenObjectType, a.UseByObjectSchema, a.DependentObjectSchema,
           (b.Level + 1) AS Level
        FROM CTE_DependentObjects a
        INNER JOIN  CTE_DependentObjects2 b 
-          ON a.UsedByObjectName = b.DependentObjectName
+         ON b.UsedByObjectId = a.DependentObjectId
+		 --and a.UsedByObjectName = b.DependentObjectName AND a.UseByObjectSchema = b.DependentObjectSchema
     )
-    SELECT DISTINCT * FROM CTE_DependentObjects2 
-    ORDER BY Level, DependentObjectName    
+    SELECT DISTINCT '    <include file="'+UseByObjectSchema+'.'+UsedByObjectName+'.sql" relativeToChangelogFile="true" />', m.Level
+	FROM CTE_DependentObjects2 m
+	WHERE NOT EXISTS(
+		SELECT 1 FROM CTE_DependentObjects2 m2 
+					 WHERE m2.level > m.level 
+					 AND m.UsedByObjectId = m2.UsedByObjectId
+		  )
+
+    ORDER BY m.Level   
 	OPTION (MAXRECURSION 32767);
